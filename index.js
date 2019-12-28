3734bfab31040c87d4c77dee33f862eff6cb4860
Katerina Zhadko <katerinkaj@icloud.com>
2019-12-28 04:06:12 +0300

    style: optimize project structure

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+$('.menu-button').on('click', function(){
+    $(this).toggleClass('opened');
+    $('.burger-menu').toggleClass('opened');
+});
+
 $('.search-button').on('click', function(){
     $(this).toggleClass('opened');
     $('.search-page').toggleClass('opened');
--- js/burger.js
+++
@@ -1,4 +0,0 @@
-$('.menu-button').on('click', function(){
-    $(this).toggleClass('opened');
-    $('.burger-menu').toggleClass('opened');
-});
